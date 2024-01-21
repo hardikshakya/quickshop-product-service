8879084e1fed60618c542607b7d587ccd812e1cb
hardikshakya <hardikshakya1997@gmail.com>
2024-01-21 10:46:32 +0530

    feat(product): added controller and service functions

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Inject } from '@nestjs/common';
+import { GrpcMethod } from '@nestjs/microservices';
 
-@Controller('product')
-export class ProductController {}
+import {
+  CreateProductRequestDto,
+  FindOneRequestDto,
+  DecreaseStockRequestDto,
+} from './product.dto';
+import {
+  CreateProductResponse,
+  FindOneResponse,
+  PRODUCT_SERVICE_NAME,
+  DecreaseStockResponse,
+} from './product.pb';
+import { ProductService } from './product.service';
+
+@Controller()
+export class ProductController {
+  @Inject(ProductService)
+  private readonly service: ProductService;
+
+  @GrpcMethod(PRODUCT_SERVICE_NAME, 'CreateProduct')
+  private createProduct(
+    payload: CreateProductRequestDto,
+  ): Promise<CreateProductResponse> {
+    return this.service.createProduct(payload);
+  }
+
+  @GrpcMethod(PRODUCT_SERVICE_NAME, 'FindOne')
+  private findOne(payload: FindOneRequestDto): Promise<FindOneResponse> {
+    return this.service.findOne(payload);
+  }
+
+  @GrpcMethod(PRODUCT_SERVICE_NAME, 'DecreaseStock')
+  private decreaseStock(
+    payload: DecreaseStockRequestDto,
+  ): Promise<DecreaseStockResponse> {
+    return this.service.decreaseStock(payload);
+  }
+}
