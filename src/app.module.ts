1b5d3e1cee7e09f9c6a9e1bc9d5d10cc47c6035d
hardikshakya <hardikshakya1997@gmail.com>
2024-01-21 10:49:20 +0530

    feat: added TypeORM configuration and added GRPC transport

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [ProductModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as any,
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      entities: ['dist/**/*.entity.{ts,js}'],
+      synchronize: process.env.TYPEORM_SYNC === 'true', // never true in production!
+    }),
+    ProductModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
+import { INestMicroservice, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
+import { join } from 'path';
+import { config } from 'dotenv';
+
+config();
+
 import { AppModule } from './app.module';
+import { protobufPackage } from './product/product.pb';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app: INestMicroservice = await NestFactory.createMicroservice(
+    AppModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        url: '0.0.0.0:50053',
+        package: protobufPackage,
+        protoPath: join(
+          'node_modules/quickshop-shared-proto/proto/product.proto',
+        ),
+      },
+    },
+  );
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  await app.listen();
 }
+
 bootstrap();
